12c442ce88b3bdb96770d712497e300c555752d2
KevinFan <kevin_ypfan@yahoo.com.tw>
2018-04-21 19:05:44 +0800

    origin finish

---
+++ client/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import query from '../queries/CurrentUser';
+import mutation from '../mutations/Login';
+import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router'
+import { connect } from 'http2';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors });
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
+      </div>
+    )
+  }
+}
+
+export default graphql(mutation)(graphql(query)(LoginForm));
---
+++ server/schema/mutations.js
@@ -0,0 +1,44 @@
+const {
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql');
+
+const UserType = require('./types/user_type');
+const AuthServies = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthServies.signup({ email, password, req });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthServies.login({ email, password, req });
+      }
+    }
+  }
+})
+
+
+module.exports = mutation;
